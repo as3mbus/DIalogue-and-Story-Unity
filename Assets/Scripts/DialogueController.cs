8c09de76c153fe16a1aeed39993db985d8bc9d6b
Lab mulmed <labmulmed123@gmail.com>
2017-08-24 14:30:20 +0700

    add dialogue script and placeholder image and animation

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using LitJson;
+
+public class DialogueController : MonoBehaviour
+{
+    public GameObject[] Chara;
+    public GameObject dPanel;
+    public Text dName, dText;
+    public string[] dialogueLines;
+    public int currentLine = 0;
+
+    // Use this for initialization
+    void Awake()
+    {
+
+    }
+    void Start()
+    {
+        currentLine=0;
+        showDialogue(dialogueLines[currentLine]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (currentLine < dialogueLines.Length - 1)
+            {
+                currentLine++;
+                showDialogue(dialogueLines[currentLine]);
+            }
+            else
+            {
+                hideDialogue();
+            }
+
+        }
+    }
+    public void showDialogue(string dialogue)
+    {
+        dPanel.SetActive(true);
+        dText.text = dialogue;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void hideDialogue()
+    {
+        dPanel.SetActive(false);
+    }
+
+}
