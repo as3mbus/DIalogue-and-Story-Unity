6cff2da5e73efa59b0a651decf7df34784a321b7
Lab Mulmed <labmulmed123@gmail.com>
2017-09-12 13:24:19 +0700

    new phase interactivity (not built tested yet)

--- Assets/Scripts/Creator/StoryCreatorControl.cs
+++ Assets/Scripts/Creator/StoryCreatorControl.cs
@@ -1,31 +1,71 @@
 ﻿using System.Collections;
+using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class StoryCreatorControl : MonoBehaviour {
-	Story targetStory;
-	public GameObject typeCanvas;
-	// Use this for initialization
-	void Start () {
-		targetStory=new Story();
-	}
+public class StoryCreatorControl : MonoBehaviour
+{
+    Story targetStory;
+    public GameObject typeCanvas, phaseScrollView;
+    public InputField storyNameField;
+    public Dropdown comicDropdown;
+
+    // Use this for initialization
+    void Start()
+    {
+        targetStory = new Story();
+        loadComics();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void newPhase()
+    {
+        typeWindowActive(true);
+
+
+    }
+    public void newDialogue()
+    {
+        targetStory.phase.Add(new Dialogue());
+        typeWindowActive(false);
+
+    }
+    public void newComic()
+    {
+        typeWindowActive(false);
+    }
+    public void cancelPhase()
+    {
+        typeWindowActive(false);
+    }
+    void typeWindowActive(bool mode)
+    {
+        typeCanvas.SetActive(mode);
+        foreach (Transform child in phaseScrollView.transform.Find("Viewport").transform.Find("Content").transform)
+        {
+            if (child.name.Contains("Content"))
+            {
+                child.GetChild(0).GetComponent<Button>().interactable = !mode;
+                child.GetChild(1).GetComponent<Button>().interactable = !mode;
+            }
+            else
+            {
+                child.GetComponent<Button>().interactable = !mode;
+            }
+        }
+    }
+    void loadComics()
+    {
+        var info = new DirectoryInfo(System.IO.Directory.GetCurrentDirectory() + "\\Assets\\Resources\\Comic");
+        // print(info);
+        var fileInfo = info.GetDirectories();
+        // storyNameField.text = "Success";
+        foreach (DirectoryInfo dir in fileInfo) comicDropdown.options.Add(new Dropdown.OptionData(dir.Name));
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	public void newPhase(){
-		typeCanvas.SetActive(true);
-	}
-	public void newDialogue(){
-		targetStory.phase.Add(new Dialogue());
-		typeCanvas.SetActive(false);
-	}
-	public void newComic(){
-		typeCanvas.SetActive(false);
-	}
-	public void cancelPhase(){
-		typeCanvas.SetActive(false);
-	}
-
 }
