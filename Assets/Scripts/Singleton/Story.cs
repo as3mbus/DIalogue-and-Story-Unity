31bb2587bf7dbc283edc81d32bce1456a00ff5e8
as3mbus <asembus@gmail.com>
2017-09-25 15:43:47 +0700

    more cariative load

--- Assets/Scripts/Singleton/Story.cs
+++ Assets/Scripts/Singleton/Story.cs
@@ -4,64 +4,97 @@ using System.Text;
 using System.IO;
 using UnityEngine;
 using LitJson;
+using as3mbus.Story;
 
-public class Story
+namespace as3mbus.Story
 {
-    public string name;
-    public ArrayList phases = new ArrayList();
-    public Story()
+    public class Story
     {
-
-    }
-    public string toJson()
-    {
-        StringBuilder sb = new StringBuilder();
-        JsonWriter writer = new JsonWriter(sb);
-        writer.PrettyPrint = true;
-        writer.IndentValue = 4;
-
-        toJson(writer);
-        return sb.ToString();
-    }
-    public void toJson(JsonWriter writer)
-    {
-        writer.WriteObjectStart();
-        writer.WritePropertyName("name");
-        writer.Write(this.name);
-        writer.WritePropertyName("phase");
-        writer.WriteArrayStart();
-
-        foreach (var phase in phases)
+        public string name;
+        public ArrayList phases = new ArrayList();
+        public Story()
         {
-            Phase fase = (Phase)phase;
-            fase.toJson(writer);
+
         }
-        writer.WriteArrayEnd();
-        writer.WriteObjectEnd();
+        public Story(storyDataType storyType)
+        {
+            switch (storyType)
+            {
+                case storyDataType.BundlePath:
+                    break;
+                case storyDataType.DataPath:
+                    break;
+                case storyDataType.JsonData:
+                    loadJsonStory(StoryManager.json);
+                    break;
+                case storyDataType.String:
+                    loadJsonStory(StoryManager.stringOrPath);
+                    break;
+                case storyDataType.TextAsset:
+                    loadJsonStory(StoryManager.textAsset);
+                    break;
+                default:
+                    break;
+            }
+        }
+        public string toJson()
+        {
+            StringBuilder sb = new StringBuilder();
+            JsonWriter writer = new JsonWriter(sb);
+            writer.PrettyPrint = true;
+            writer.IndentValue = 4;
+
+            toJson(writer);
+            return sb.ToString();
+        }
+        public void toJson(JsonWriter writer)
+        {
+            writer.WriteObjectStart();
+            writer.WritePropertyName("name");
+            writer.Write(this.name);
+            writer.WritePropertyName("phase");
+            writer.WriteArrayStart();
+
+            foreach (var phase in phases)
+            {
+                Phase fase = (Phase)phase;
+                fase.toJson(writer);
+            }
+            writer.WriteArrayEnd();
+            writer.WriteObjectEnd();
+        }
+
+        public Story(string filePath)
+        {
+            this.name = Path.GetFileNameWithoutExtension(filePath);
+            JsonData jsonStory = JsonMapper.ToObject(File.ReadAllText(filePath));
+            loadJsonStory(jsonStory);
+        }
+
+        public Story(TextAsset storyJson)
+        {
+            loadJsonStory(storyJson);
+        }
+        void loadJsonStory(TextAsset storyJson)
+        {
+            this.name = storyJson.name;
+            loadJsonStory(storyJson.text);
+        }
+        void loadJsonStory(string storyJsonString)
+        {
+            JsonData jsonStory = JsonMapper.ToObject(storyJsonString);
+            loadJsonStory(jsonStory);
+        }
+        void loadJsonStory(JsonData storyJson)
+        {
+            this.name = storyJson["name"].ToString();
+            foreach (JsonData cerita in storyJson["phase"])
+            {
+                Phase fase = new Phase(cerita);
+                this.phases.Add(fase);
+            }
+        }
+
     }
 
-    public Story(string filePath)
-    {
-        this.name = Path.GetFileNameWithoutExtension(filePath);
-        JsonData jsonStory = JsonMapper.ToObject(File.ReadAllText(filePath));
-        loadJsonStory(jsonStory);
-    }
-    public Story (TextAsset storyJson){
-        loadTextAsset(storyJson);
-    }
-    public void loadTextAsset(TextAsset storyJson)
-    {
-        this.name=storyJson.name;
-        JsonData jsonStory = JsonMapper.ToObject(storyJson.ToString());
-        loadJsonStory(jsonStory);
-    }
-    void loadJsonStory(JsonData storyJson)
-    {
-        this.name = storyJson["name"].ToString();
-        foreach (JsonData cerita in storyJson["phase"])
-        {
-            Phase fase = new Phase(cerita);
-            this.phases.Add(fase);
-        }
-    }
 }
