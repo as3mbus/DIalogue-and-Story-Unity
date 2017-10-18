d0f1775cc7d2e84e4507d6ab633fcadcf783022b
as3mbus <asembus@gmail.com>
2017-10-19 00:14:49 +0700

    change on gitignore and using on Simple file browsergit. and some kelyll config change

--- Assets/Scripts/Creator/StoryCreator.cs
+++ Assets/Scripts/Creator/StoryCreator.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using as3mbus.Story;
-using GracesGames;
+using SimpleFileBrowser.Scripts.GracesGames;
 
 public class StoryCreator : MonoBehaviour
 {
