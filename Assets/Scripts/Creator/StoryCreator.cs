e26b42ef30e7e7279e99ec4ad0b4102c59877bbf
as3mbus <asembus@gmail.com>
2017-10-24 14:15:47 +0700

    revert namespace for file browser and update docs and simple file browser

--- Assets/Scripts/Creator/StoryCreator.cs
+++ Assets/Scripts/Creator/StoryCreator.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using as3mbus.Story;
-using SimpleFileBrowser.Scripts.GracesGames;
+using GracesGames;
 
 public class StoryCreator : MonoBehaviour
 {
