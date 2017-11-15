d0d7f20ffa96efba454aadef9ba46fc4bfb76e91
as3mbus <asembus@gmail.com>
2017-11-15 23:17:24 +0700

    persistent data path scene test. which is internal app storage of android application

---
+++ Assets/Scripts/checkPersistentDataPath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class checkPersistentDataPath : MonoBehaviour {
+	public InputField messageField;
+
+	// Use this for initialization
+	void Start () {
+		print("datapath : "+Application.persistentDataPath);
+		messageField.text = "datapath : "+Application.persistentDataPath;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
